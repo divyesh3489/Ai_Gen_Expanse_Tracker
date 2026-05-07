b23c103739884648e3a827b29089b9ace65e4664
Divyesh <divyeshgurjar2004@gmail.com>
2026-05-07 19:52:16 +0530

    feat: configure static file handling with Nginx and update settings for static files

--- ExpanseTraker/ExpanseTraker/settings.py
+++ ExpanseTraker/ExpanseTraker/settings.py
@@ -222,6 +222,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files (user uploads / publicly served assets)
 MEDIA_URL = "/media/"
