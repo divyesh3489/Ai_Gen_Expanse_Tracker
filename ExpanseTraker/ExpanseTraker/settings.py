331aac185d2d94febb0e0581fb2631ef4ee07fd7
Divyesh <divyeshgurjar2004@gmail.com>
2026-05-07 19:48:41 +0530

    fix: update STATIC_URL to include leading slash for correct static file routing

--- ExpanseTraker/ExpanseTraker/settings.py
+++ ExpanseTraker/ExpanseTraker/settings.py
@@ -221,7 +221,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Media files (user uploads / publicly served assets)
 MEDIA_URL = "/media/"
